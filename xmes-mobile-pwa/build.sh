079eef8d431d009513e3316232af69becd965a86
flopetautschnig <fp-others@mailbox.org>
2026-04-22 22:46:43 +0200

    fix[WIP]: build.sh

--- xmes-mobile-pwa/build.sh
+++ xmes-mobile-pwa/build.sh
@@ -6,6 +6,9 @@ rustup target add wasm32-unknown-unknown
 
 cargo install dioxus-cli
 
+which clang clang-14 clang-15 clang-16 clang-17 clang-18 2>/dev/null || echo "no clang found"
+ls /usr/bin/clang* 2>/dev/null || echo "no clang in /usr/bin"
+
 sudo apt install -y clang libzstd-dev
 
 dx build --release --web
