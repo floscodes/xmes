712ff1f0f9c86b22c1d1405e415f0e9f352ad739
floscodes <mail@floscodes.net>
2026-04-27 11:24:01 +0000

    chore: update gitignore, tailwind.css and identities component tweaks

--- xmes-mobile-pwa/src/components/identities/mod.rs
+++ xmes-mobile-pwa/src/components/identities/mod.rs
@@ -549,7 +549,7 @@ fn IdentityCard(
                             rect { x: "3", y: "11", width: "18", height: "11", rx: "2", ry: "2" }
                             path { d: "M7 11V7a5 5 0 0 1 10 0v4" }
                         }
-                        span { "Share with other device" }
+                        span { "Share identity with other device" }
                     }
                 }
 
